1e44912d3179842e4fcaeb7cd6dd6feef5b55504
ja-odur <odurjoseph8@gmail.com>
2025-06-27 22:59:22 +0300

    Configure dev abd test dbs

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :rumbl, Rumbl.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "ecobet",
+  password: "ecobet",
   hostname: "localhost",
-  database: "rumbl_dev",
+  database: "rumbl",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
