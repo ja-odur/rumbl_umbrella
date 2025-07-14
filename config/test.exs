f54d6fa011e35b412c05c89b0d95d6c11a224a12
ja-odur <odurjoseph8@gmail.com>
2025-07-14 22:25:04 +0300

    Add test http client

--- config/test.exs
+++ config/test.exs
@@ -35,3 +35,7 @@ config :phoenix, :plug_init_mode, :runtime
 # Enable helpful, but potentially expensive runtime checks
 config :phoenix_live_view,
   enable_expensive_runtime_checks: true
+
+config :info_sys, :wolfram,
+  app_id: "1234",
+  http_client: InfoSys.Test.HTTPClient
